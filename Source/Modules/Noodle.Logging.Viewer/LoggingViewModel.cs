b746e9d350b1456e16a72ef3162105d7aadce347
Paul Knopf <theonlylawislove@gmail.com>
2013-02-12 21:26:08 -0500

    Working on the log viewer

--- Source/Modules/Noodle.Logging.Viewer/LoggingViewModel.cs
+++ Source/Modules/Noodle.Logging.Viewer/LoggingViewModel.cs
@@ -8,6 +8,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using MongoDB.Driver;
 using Noodle.Data;
+using SimpleInjector;
 
 namespace Noodle.Logging.Viewer
 {
@@ -20,6 +21,9 @@ namespace Noodle.Logging.Viewer
         private MongoCollection<Log> _logCollection;
         private MongoDatabase _database;
         private bool _isConnected;
+        private int _totalPages;
+        private int _currentPage;
+        private IPagedList<Log> _currentSet;
 
         /// <summary>
         /// Ctor
@@ -29,7 +33,19 @@ namespace Noodle.Logging.Viewer
             if (!IsInDesignMode)
             {
                 ConnectCommand = new RelayCommand(Connect);
+                ForwardCommand = new RelayCommand(Forward, () => _currentSet != null && _currentSet.HasPreviousPage);
+                BackCommand = new RelayCommand(Back, () => _currentSet != null && _currentSet.HasNextPage);
             }
+        }
+
+        private void Back()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Forward()
+        {
+            throw new NotImplementedException();
         }
 
         public ObservableCollection<Log> Logs
@@ -79,11 +95,17 @@ namespace Noodle.Logging.Viewer
         /// <summary>
         /// Estabish a connection to the server
         /// </summary>
-        public RelayCommand ConnectCommand
-        {
-            get { return _connectCommand; }
-            set { _connectCommand = value; }
-        }
+        public RelayCommand ConnectCommand { get; protected set; }
+
+        /// <summary>
+        /// Go back
+        /// </summary>
+        public RelayCommand BackCommand { get; protected set; }
+
+        /// <summary>
+        /// Go forward
+        /// </summary>
+        public RelayCommand ForwardCommand { get; protected set; }
 
         /// <summary>
         /// Try to connect to the server
@@ -95,6 +117,7 @@ namespace Noodle.Logging.Viewer
                 _database = new MongoDB.MongoService(new SqlConnectionProvider(ConnectionString)).GetDatabase(databaseName: DatabaseName);
                 _logCollection = _database.GetCollection<Log>("Log");
                 IsConnected = true;
+                Refresh();
             }
             catch (Exception ex)
             {
@@ -103,6 +126,61 @@ namespace Noodle.Logging.Viewer
             }
         }
 
-        
+        /// <summary>
+        /// The total number of pages
+        /// </summary>
+        public int TotalPages
+        {
+            get { return _totalPages; }
+            set
+            {
+                _totalPages = value;
+                RaisePropertyChanged(() => TotalPages);
+            }
+        }
+
+        /// <summary>
+        /// The current page
+        /// </summary>
+        public int CurrentPage
+        {
+            get { return _currentPage; }
+            set
+            {
+                if (_totalPages <= 0)
+                    _currentPage = -1;
+                else
+                {
+                    if (value < 0)
+                    {
+                        _currentPage = 1;
+                    }
+                    else if (value > TotalPages)
+                    {
+                        _currentPage = TotalPages;
+                    }
+                    else
+                    {
+                        _currentPage = TotalPages;
+                    }
+                }
+                RaisePropertyChanged(() => CurrentPage);
+            }
+        }
+
+        public void Refresh()
+        {
+            if (_currentSet == null || (_currentPage - 1) != _currentSet.PageIndex)
+            {
+                _currentSet = new DefaultLogger(new Container(), _logCollection).GetAllLogs(pageIndex: _currentPage);
+                _currentPage = _currentSet.PageIndex + 1;
+                TotalPages = _currentSet.TotalPages;
+                _logs.Clear();
+                foreach(var log in _currentSet)
+                {
+                    _logs.Add(log);
+                }
+            }
+        }
     }
 }
