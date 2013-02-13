04322339808b9b48137176cda8f1d780aae9c302
Paul Knopf <theonlylawislove@gmail.com>
2013-02-13 16:16:36 -0500

    Adding copy/paste to the xml editor for languages

--- Source/Modules/Noodle.Localization.XmlEditor/MainWindow.xaml.cs
+++ Source/Modules/Noodle.Localization.XmlEditor/MainWindow.xaml.cs
@@ -17,11 +17,31 @@ namespace Noodle.Localization.XmlEditor
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            LanguagesTab.PreviewMouseDown += LanguagesTabOnPreviewMouseDown;
+            Focusable = true;
+        }
+
+        private void LanguagesTabOnPreviewMouseDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            // this fixes an issue in windows xp 
+            // see http://stackoverflow.com/questions/5223998/wpf-datagrid-itemssource-binding-issue
+            Focus();
+            LanguagesTab.Focus();
+        }
+
+        private void DataGrid_PreparingCellForEdit(object sender, DataGridPreparingCellForEditEventArgs e)
+        {
+            TextBox tb = e.EditingElement as TextBox;
+            if (tb != null)
+            {
+                tb.Focus();
+                //you can set caret position and ...
+            }
         }
     }
 }
