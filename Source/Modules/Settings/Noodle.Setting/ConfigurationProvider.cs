d378e52ff15f70553192ab795f73d3c563d45654
Paul Knopf <theonlylawislove@gmail.com>
2014-05-30 11:17:18 -0400

    Modify the IConfigurationProvider to support listeners to know when others have saved the settings.

--- Source/Modules/Settings/Noodle.Setting/ConfigurationProvider.cs
+++ Source/Modules/Settings/Noodle.Setting/ConfigurationProvider.cs
@@ -3,40 +3,97 @@ using System.Linq;
 
 namespace Noodle.Settings
 {
+    /// <summary>
+    /// See IConfigurationProvider
+    /// </summary>
+    /// <typeparam name="TSettings"></typeparam>
     public class ConfigurationProvider<TSettings> : IConfigurationProvider<TSettings>, IDisposable where TSettings : ISettings, new()
     {
+        #region Fields
+
         readonly ISettingService _settingService;
 
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationProvider{TSettings}"/> class.
+        /// </summary>
+        /// <param name="settingService">The setting service.</param>
         public ConfigurationProvider(ISettingService settingService)
         {
             _settingService = settingService;
 
             // assigned events so we can update our object if needed
-            _settingService.CachedCleared += SettingServiceOnCachedCleared;
+            _settingService.CachedCleared += OnSettingsCachedCleared;
 
             BuildConfiguration();
         }
 
+        #endregion
+
+        #region IConfigurationProvider
+
+        /// <summary>
+        /// Gets or sets the settings.
+        /// </summary>
+        /// <value>
+        /// The settings.
+        /// </value>
         public TSettings Settings { get; protected set; }
 
+        /// <summary>
+        /// Builds the configuration.
+        /// </summary>
         private void BuildConfiguration()
         {
             Settings = _settingService.GetSetting<TSettings>();
         }
 
+        /// <summary>
+        /// Saves the settings.
+        /// </summary>
         public void SaveSettings()
         {
             _settingService.SaveSetting(Settings);
+
+            var settingsChangedHandler = SettingsChanged;
+            if (settingsChangedHandler != null)
+                settingsChangedHandler(this, EventArgs.Empty);
         }
 
-        private void SettingServiceOnCachedCleared(object sender, EventArgs eventArgs)
-        {
-            BuildConfiguration();
-        }
+        /// <summary>
+        /// Occurs when [settings changed].
+        /// </summary>
+        public event EventHandler SettingsChanged;
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
-            _settingService.CachedCleared -= SettingServiceOnCachedCleared;
+            _settingService.CachedCleared -= OnSettingsCachedCleared;
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Raised when someone has clear the settings cache
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void OnSettingsCachedCleared(object sender, EventArgs eventArgs)
+        {
+            BuildConfiguration();
+
+            var settingsChangedHandler = SettingsChanged;
+            if (settingsChangedHandler != null)
+                settingsChangedHandler(this, EventArgs.Empty);
+        }
+
+        #endregion
     }
 }
--- Source/Modules/Settings/Noodle.Setting/IConfigurationProvider.cs
+++ Source/Modules/Settings/Noodle.Setting/IConfigurationProvider.cs
@@ -1,8 +1,26 @@
-﻿namespace Noodle.Settings
+﻿using System;
+
+namespace Noodle.Settings
 {
+    /// <summary>
+    /// Used to strongly type/reference a settings object for modification and saving
+    /// </summary>
+    /// <typeparam name="TSettings"></typeparam>
     public interface IConfigurationProvider<TSettings> where TSettings : ISettings, new()
     {
+        /// <summary>
+        /// The instance to the settings object
+        /// </summary>
         TSettings Settings { get; }
+
+        /// <summary>
+        /// Save this settings object to the database
+        /// </summary>
         void SaveSettings();
+
+        /// <summary>
+        /// Raised when someone calls "SaveSettings", or when someone clears the cache
+        /// </summary>
+        event EventHandler SettingsChanged;
     }
 }
